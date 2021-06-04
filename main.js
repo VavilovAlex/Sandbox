0056c6b4ea04f7cfd07df18db8da0a0ecd3a524c
StrangePerch <sasha.aav.sv@gmail.com>
2021-06-04 10:42:19 +0300

    Water added Fixed update Optimised draw

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ const context = canvas.getContext('2d');
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-width = 100;
-height = 100;
+let y_mult = 2;
+let x_mult = 2;
 
-let y_mult = canvas.height / height;
-let x_mult = canvas.width / width;
+height = Math.floor(canvas.height / y_mult);
+width = Math.floor(canvas.width / x_mult);
+
+
 
 function Move(x1, y1, x2, y2)
 {
+    let buffer = world[x2][y2];
     world[x2][y2] = world[x1][y1];
-    world[x1][y1] = new Air();
+    world[x1][y1] = buffer;
+    Draw(x1, y1);
+    Draw(x2, y2);
+    world[x1][y1].updated = true;
+    world[x2][y2].updated = true;
 }
 
 function Draw(x, y) {
@@ -27,6 +34,7 @@ function Draw(x, y) {
 class Air
 {
     color = "#ffffff";
+    solidity = 0;
     
     Update() {}
 }
@@ -34,6 +42,7 @@ class Air
 class Sand {
     color = "#ffe855";
     updated = false;
+    solidity = 3;
     
     Update(x, y) {
         if (this.updated === true){
@@ -42,19 +51,72 @@ class Sand {
         this.updated = true;
         
         if(y === height - 1) return;
-        if(world[x][y + 1] instanceof Air)
+        if(world[x][y + 1].solidity < this.solidity)
         {
             Move(x, y, x, y + 1);
         }
         else{
-            if (x > 0 && world[x - 1][y + 1] instanceof Air) {
+            if (x > 0 
+                && world[x - 1][y + 1].solidity < this.solidity
+                && world[x - 1][y].solidity < this.solidity) {
                 Move(x, y, x - 1, y + 1);
-            } else if (x < width - 1 && world[x + 1][y + 1] instanceof Air) {
+            } else if (
+                x < width - 1 
+                && world[x + 1][y + 1].solidity < this.solidity
+                && world[x + 1][y].solidity < this.solidity) {
                 Move(x, y, x + 1, y + 1);
             }
         }
         
     }
+}
+
+class Water {
+    color = "#2254ff";
+    updated = false;
+    solidity = 2;
+    
+    Update(x, y) {
+        if (this.updated === true){
+            return;
+        }
+        
+        if(y === height - 1) return;
+        if(world[x][y + 1].solidity < this.solidity)
+        {
+            Move(x, y, x, y + 1);
+        }
+        else{
+            if (x > 0
+                && world[x - 1][y + 1].solidity < this.solidity
+                && world[x - 1][y].solidity < this.solidity) {
+                Move(x, y, x - 1, y + 1);
+            } else if (
+                x < width - 1
+                && world[x + 1][y + 1].solidity < this.solidity
+                && world[x + 1][y].solidity < this.solidity) {
+                Move(x, y, x + 1, y + 1);
+            }
+            else if(
+                x > 0
+                && world[x - 1][y].solidity < this.solidity)
+            {
+                Move(x, y, x - 1, y);
+            }
+            else if(
+                x < width - 1
+                && world[x + 1][y].solidity < this.solidity)
+            {
+                Move(x, y, x + 1, y);
+            }
+        }
+
+    }
+}
+
+function MouseMove()
+{
+    
 }
 
 function UpdateWorld() {
@@ -70,9 +132,9 @@ function UpdateWorld() {
         }
     }
 
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            Draw(x,y);
+    for (let x = width - 1; x >= 0; x--){
+        for (let y = height - 1; y >= 0; y--) {
+            world[x][y].Update(x, y);
         }
     }
 }
@@ -88,9 +150,17 @@ for (let x = 0; x < width; x++) {
 
 for (let j = 0; j < 50; j++) {
     for (let i = 0; i < 40; i++) {
+        if(i % 2 === 0 ^ j % 2 === 0)
         world[30 + j][i] = new Sand();
     } 
 }
 
+for (let j = 0; j < 50; j++) {
+    for (let i = 0; i < 40; i++) {
+        if(i % 2 === 0 ^ j % 2 === 0)
+            world[90 + j][i] = new Water();
+    }
+}
 
-let interval = setInterval(UpdateWorld, 100)
+
+let interval = setInterval(UpdateWorld, 10)
